0ac5c2098ae1395843cb6f3e32bbdb25fc52dca9
YonatanBenYaakov <yonatanby2008@gmail.com>
2025-06-30 20:38:05 +0300

    new changes

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 def file_to_dict(file_path):
+    """
+    פותח קובץ טקסט ומחזיר מילון שבו כל מילה ומספר ההופעות שלה.
+
+    :param file_path: נתיב לקובץ הטקסט
+    :return: מילון {מילה: מספר הופעות}
+    """
+     
     dic = {}
     with open(file_path) as f:  #פותח את הקובץ טקסט
         for line in f:         
@@ -10,8 +17,15 @@ def file_to_dict(file_path):
                     dic[word] = 1 # מיקום במילון של המילה שמופיעה פעם אחת
     return dic
 
-def sort_dict(dic, n):
-    sorted_dict = dict(sorted(dic.items(), key=lambda word: word[1], reverse=True)[:n])
+def sort_dict(dic, n): 
+    """
+    מקבלת מספר ומילון מסדרת את המילון ונותן את המספר של המילים שחש העח הרבה.
+
+    :param file_path: מילון,מספר
+    :return: מילון מסודר {מילה: מספר הופעות}
+    """
+     
+    sorted_dict = dict(sorted(dic.items(), key=lambda word: word[1], reverse=True)[:n]) # צריך לעשות ריוורס כדי שיהיה מהגדול לקטן
     return sorted_dict
 
 file_path = input("enter file name ") #מקבל path ומספר
